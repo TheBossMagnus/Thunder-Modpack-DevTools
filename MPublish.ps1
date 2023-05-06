45b46c92283b021ffbf28647dc438fff19370649
TheBossMagnus <92474350+TheBossMagnus@users.noreply.github.com>
2023-05-06 19:15:03 +0200

    fix

--- MPublish.ps1
+++ MPublish.ps1
@@ -4,7 +4,7 @@ $version = (Get-ChildItem -Path "C:\Users\User\GitHub\Quilt-Optimized\bin" -Dire
 gh release create $version -R TheBossMagnus/Quilt-Optimized -t $version -d
 
 #iterate through all the mrpack files in the latest version folder and upload them to the release
-Get-ChildItem -Path "C:\Users\User\GitHub\Quilt-Optimized\bin\$version" -Filter "*.mrpack" -Recurse | ForEach-Object {
+Get-ChildItem -Path "C:\Users\User\GitHub\Quilt-Optimized\bin\$version" -Include "*.mrpack", "*.md" -Recurse | ForEach-Object {
     gh release upload $version $_.FullName
 }
 

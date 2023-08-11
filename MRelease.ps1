334529e7f0a863db11c63adeb014f5acf10796fd
TheBossMagnus <92474350+TheBossMagnus@users.noreply.github.com>
2023-08-11 19:35:34 +0200

    Preliminary work to support new versionig schema

--- MRelease.ps1
+++ MRelease.ps1
@@ -4,6 +4,18 @@ $devName = $args[3]
 $root = $args[5]
 $editions = $args[7..($args.Length - 1)]
 
+# Prompt the user to enter the target MC version
+#$targetMc = Read-Host "Enter the target MC version"
+
+# Filter the editions array based on the input
+#$editions = $editions | Where-Object { $_.Split('/')[1] -eq $targetMc }
+
+# Check if any editions were found
+#if ($editions.Count -eq 0) {
+#    Write-Error "No editions found for MC version $targetMc"
+#    exit 1
+#}
+
 # Prompt the user to enter the release number
 $release = Read-Host "Enter the release number"
 
@@ -11,9 +23,10 @@ $release = Read-Host "Enter the release number"
 $olderVersion = (Get-ChildItem -Path "$root\bin" -Directory | Sort-Object LastWriteTime)[-1].Name
 
 # Create folders for .mrpack files and changelogs
-New-Item -Path "$root\bin\$release" -Name "quilt" -ItemType "directory" | Out-Null
-New-Item -Path "$root\bin\$release" -Name "fabric" -ItemType "directory" | Out-Null
-New-Item -Path "$root\bin\$release" -Name "changelog" -ItemType "directory" | Out-Null
+$binPath = "$root\bin\$release"
+New-Item -Path $binPath -Name "quilt" -ItemType "directory" | Out-Null
+New-Item -Path $binPath -Name "fabric" -ItemType "directory" | Out-Null
+New-Item -Path $binPath -Name "changelog" -ItemType "directory" | Out-Null
 
 # Iterate through all the editions and generate .mrpack files and changelogs
 foreach ($edition in $editions) {
@@ -21,8 +34,7 @@ foreach ($edition in $editions) {
     Set-Location -Path "$root\src\$edition"
 
     # Get the modloader and Minecraft version
-    $loader = $edition.split("/")[0]
-    $MCversion = $edition.split("/")[1]
+    $loader, $MCversion = $edition.Split('/')
 
     # Update pack.toml
     packwiz init -r --name $modpackName --author $devName --modloader $loader --$loader-latest --version $release --mc-version $MCversion | Out-Null
@@ -31,32 +43,27 @@ foreach ($edition in $editions) {
     packwiz mr export | Out-Null
 
     # Rename .mrpack and move it to the bin folder
-    Move-Item -Path "$modpackName-$release.mrpack" -destination "$root\bin\$release\$loader\$modpackName-$release for $Loader $MCversion.mrpack" | Out-Null
+    $newName = "$modpackName-$release for $loader $MCversion.mrpack"
+    Move-Item -Path "$modpackName-$release.mrpack" -Destination "$binPath\$loader\$newName" | Out-Null
 
     # Generate the paths of the current and older .mrpack files
-    $currentPack = "$root\bin\$release\$loader\$modpackName-$release for $loader $MCversion.mrpack"
+    $currentPack = "$root\bin\$release\$loader\$newName"
     $oldPack = "$root\bin\$olderVersion\$loader\$modpackName-$olderVersion for $loader $MCversion.mrpack"
 
     # Create a blank changelog file
-    New-Item -Path "$root\bin\$release\changelog" -Name "$Loader $MCversion Changelog.md" -ItemType "file" | Out-Null
+    New-Item -Path "$root\bin\$release\changelog" -Name "$loader $MCversion Changelog.md" -ItemType "file" | Out-Null
 
     if ((Test-Path -Path $oldPack) -and (Test-Path -Path $currentPack)) {
         # Generate the changelog
-        java -jar "C:\Users\User\GitHub\Modpack DevTools\ModListCreator.jar" changelog -old $oldPack -new $currentPack -out "$root\bin\$release\changelog\$Loader $MCversion Changelog.md"
+        java -jar "C:\Users\User\GitHub\Modpack DevTools\ModListCreator.jar" changelog -old $oldPack -new $currentPack -out "$root\bin\$release\changelog\$loader $MCversion Changelog.md"
     }
     else {
         # If the .mrpack file doesn't exist, write "No changelog available" and print a warning
-        Set-Content "$root\bin\$release\changelog\$Loader $MCversion Changelog.md" -Value "No changelog available"
+        Set-Content "$root\bin\$release\changelog\$loader $MCversion Changelog.md" -Value "No changelog available"
         Write-Warning "No changelog was available"
     }
 
     Write-Host "`n$modpackName-$release for $loader $MCversion done" -ForegroundColor Green
 }
-
-# Add the changelog to the global changelog with a date timestamp
-$newchangelog = Get-Content "$root\bin\$release\Changelog\Fabric 1.20.1 changelog.md"
-$globalchangelog = Get-Content "$root\Changelog.md"
-$date = "> " + (Get-Date -Format "dd/MM/yyyy")
-Set-Content "$root\Changelog.md" -Value ($newchangelog + $date + "`n---`n" + $globalchangelog)
 
 Write-Host "`n`nAll done!" -ForegroundColor Green

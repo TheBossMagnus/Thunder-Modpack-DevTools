1dfb170e59f0f3f2b926ae0439dee08af8bce114
TheBossMagnus <92474350+TheBossMagnus@users.noreply.github.com>
2023-09-03 12:28:47 +0200

    First changes for thunder new versioning

--- MBulkRun.ps1
+++ MBulkRun.ps1
@@ -2,40 +2,13 @@
 $root = $args[5]
 $editions = $args[7..($args.Length - 1)]
 
-# Define the root folder to search in
-$root = "C:\Users\user\GitHub\Thunder\src"
-
-# Generate the list of subfolders to search in
-$subfolders = $editions | ForEach-Object { Join-Path $root $_ }
-
-# Print the available subfolders to the console
-Write-Host "Available subfolders"
-$counter = 1
-$editions | ForEach-Object { Write-Host "$counter. $_"; $counter++ }
-
-# Prompt the user to select subfolders to run the command in
-$selectedSubfolders = Read-Host "Enter the subfolder numbers separated by commas (or 'a' for all subfolders)"
-$selectedSubfolderNumbers = $selectedSubfolders.Split(",")
-
 # Prompt the user to enter the command to run
 $command = Read-Host "Enter the command to run in all selected subfolders:"
 
-# If the user selected all subfolders, run the command in all subfolders
-if ($selectedSubfolders -eq "a") {
-    Write-Host "Running command in all subfolders..."
-    foreach ($subfolder in $subfolders) {
-        Write-Host "Running command in $($subfolder)..."
-        Set-Location $subfolder
-        Invoke-Expression $command
-    }
-}
-# Otherwise, run the command in the selected subfolders
-else {
-    foreach ($number in $selectedSubfolderNumbers) {
+
+foreach ($edition in $editions) {
         # Get the path of the selected subfolder
-        $subfolderPath = $subfolders[$number.Trim() - 1]
-        Write-Host "Running command in $($subfolderPath)..."
-        Set-Location $subfolderPath
+        Write-Host "Running command in $($edition)..."
+        Set-Location "$root\src\$edition"
         Invoke-Expression $command
     }
-}
--- Thunder.ps1
+++ Thunder.ps1
@@ -5,23 +5,31 @@ $root = "C:\Users\User\GitHub\Thunder"
 $editions = @(
     "fabric/1.16.5",
     "fabric/1.17.1",
-    "fabric/1.18.2",
-    "fabric/1.19.2",
-    "fabric/1.19.3",
-    "fabric/1.19.4",
-    "fabric/1.20.1",
     "quilt/1.18.2",
+    "fabric/1.18.2",
     "quilt/1.19.2",
+    "fabric/1.19.2",
     "quilt/1.19.3",
+    "fabric/1.19.3",
     "quilt/1.19.4",
-    "quilt/1.20.1"
+    "fabric/1.19.4",
+    "quilt/1.20.1",
+    "fabric/1.20.1",
+    "fabric/1.20.2"
 )
+
+
+# Get the name of the sub-script to run from the command line arguments and the target edition
+$script = $args[0]
+$Target = $args[1]
+
+# If a target edition was specified, filter the editions array to only include the target edition
+if ($Target -ne "") {
+    $editions = $editions | Where-Object {$_ -Like "*$target"}
+}
 
 # Define arguments to pass to sub-scripts
 $arguments = @("-modpackName $modpackName -devName $devName -root $root -editions $editions")
-
-# Get the name of the sub-script to run from the command line arguments
-$script = $args[0]
 
 # Call the appropriate sub-script based on the command line argument
 if ($script -eq "Update" -or $script -eq "u") {
@@ -35,9 +43,6 @@ if ($script -eq "Update" -or $script -eq "u") {
 
 } elseif ($script -eq "Release" -or $script -eq "r") {
     Invoke-Expression "& `"MRelease.ps1`" $arguments"
-
-} elseif ($script -eq "Mirror-Config"  -or $script -eq "mc") {
-    Invoke-Expression "& `"MMirrorConfig.ps1`" $arguments"
 
 } elseif ($script -eq "Help"  -or $script -eq "h" -or $script -eq "?") {
     Invoke-Expression "& `"Help.ps1`" $arguments"

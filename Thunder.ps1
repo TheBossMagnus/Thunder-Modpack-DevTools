6b47239b2e6085971937821990d74118d676f0d5
TheBossMagnus <TheBossMagnus@proton.me>
2024-07-05 12:41:54 +0200

    first port of quilt on 1.20.6 and 1.21

--- Thunder.ps1
+++ Thunder.ps1
@@ -9,9 +9,11 @@ $editions = @(
 	"1.19.2\fabric",
 	"1.20.1\quilt",
 	"1.20.1\fabric",
-	"1.20.4\fabric",
 	"1.20.4\quilt",
+	"1.20.4\fabric",
+	"1.20.6\quilt",
 	"1.20.6\fabric",
+	"1.21\quilt",
 	"1.21\fabric"
 )
 

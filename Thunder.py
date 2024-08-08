61a8895f4b38dd7a41bda5abc6736a84c4c467a6
TheBossMagnus <TheBossMagnus@proton.me>
2024-08-08 21:00:14 +0200

    fix version selection
    Signed-off-by: TheBossMagnus <TheBossMagnus@proton.me>

--- Thunder.py
+++ Thunder.py
@@ -9,7 +9,7 @@ from MUpdateList import update_list
 modpack_name = "Thunder"
 modpack_author = "TheBossMagnus"
 root = r"D:\Thunder"
-editions = [
+supp_editions = [
     r"1.16.5\fabric",
     r"1.18.2\fabric",
     r"1.19.2\quilt",
@@ -27,15 +27,26 @@ editions = [
 
 def main() -> None:
     try:
-        global editions, root, modpack_name, modpack_author
+        global supp_editions, root, modpack_name, modpack_author
         tool = sys.argv[1]
 
         targets = sys.argv[2:]
-        editions = [edition for edition in editions if any(target in edition for target in targets)]
+        
+        if 'a' in targets or 'all' in targets:
+            editions = supp_editions
+        else:
+            editions = [
+                edition for edition in supp_editions
+                if any(target == part for target in targets for part in edition.split('\\'))
+            ]
+        
+        if not editions:
+            print("No edition selected")
+            return
 
         if tool in ("u", "update"):
             update(root, editions)
-        elif tool in ("ub", "updatebulk"):
+        elif tool in ("ul", "updatelist"):
             update_list(root, editions)
         elif tool in ("br", "bulkrun"):
             bulk_run(root, editions)
@@ -48,6 +59,7 @@ def main() -> None:
             build_modpack(modpack_name, modpack_author, root, editions)
             publish(root, editions, modpack_author, modpack_name)
         else:
+            print("Inavlid tool")
             return
     except KeyboardInterrupt:
         print("\nOperation cancelled by user.")

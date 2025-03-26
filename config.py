3f8412d55dda8bead4c8df85eb687033b8b7eeec
TheBossMagnus <TheBossMagnus@proton.me>
2025-03-26 17:16:38 +0100

    add support for Minecraft version 1.21.5 and improve version retrieval error handling
    Signed-off-by: TheBossMagnus <TheBossMagnus@proton.me>

--- config.py
+++ config.py
@@ -12,14 +12,19 @@ supp_editions = {
     "1.20.4": ["quilt", "fabric"],
     "1.21.1": ["quilt", "fabric"],
     "1.21.3": ["quilt", "fabric"],
-    "1.21.4": ["quilt", "fabric"]
+    "1.21.4": ["quilt", "fabric"],
+    "1.21.5": ["quilt", "fabric"],
 }
 
 packwiz_dir = "pakku"
 
+
 def get_latest_version(mc_version: str) -> str:
     """Get the latest version of the modpack for the specified Minecraft version.
-        Searches in bin folder of the specified Minecraft version for the newest dir, witch corresponds to the latest version of the modpack."""
+    Searches in bin folder of the specified Minecraft version for the dir with the most recent creation date, witch corresponds to the latest version of the modpack published as now."""
     version_path = Path(root) / "bin" / mc_version
     versions = [p for p in version_path.glob("*") if p.is_dir()]
+    if not versions:
+        print(f"No versions found for Minecraft {mc_version}, if this is the first release for this mc version, you can ignore this message.")
+        return "No older version"  # Just a random string
     return versions[-1].name

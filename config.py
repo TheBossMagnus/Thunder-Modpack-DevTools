758285a10dfec223b8114708df3cbb1578346f6e
TheBossMagnus <TheBossMagnus@proton.me>
2025-06-20 18:42:48 +0200

    update supported versions

--- config.py
+++ config.py
@@ -5,9 +5,6 @@ modpack_name = "Thunder"
 modpack_author = "TheBossMagnus"
 root = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "Thunder")
 supp_editions = {
-    "1.16.5": ["fabric"],
-    "1.18.2": ["fabric"],
-    "1.19.2": ["quilt", "fabric"],
     "1.20.1": ["quilt", "fabric"],
     "1.20.4": ["quilt", "fabric"],
     "1.21.1": ["quilt", "fabric"],
